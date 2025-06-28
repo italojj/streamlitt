9234db67d69e967a8df583de5ba4444e26fcd7cb
Italo Nascimento <58990025+italojj@users.noreply.github.com>
2025-06-27 22:33:36 -0300

    Update app.py

--- app.py
+++ app.py
@@ -14,17 +14,20 @@ try:
 
     if 'source' in df.columns and 'target' in df.columns:
         G = nx.from_pandas_edgelist(df, 'source', 'target')
-
+        minimum_degree = 3
+        nodes_selected = [n for n, d in G.degree() if d > minimum_degree]
+        G_subsubgraph = G.subgraph(nodes_selected)
+        st.info(f"Rede original: {G.number_of_nodes()} nós. | Exibindo rede reduzida (nós com mais de {minimum_degree} conexões): {G_subsubgraph.number_of_nodes()} nós.")
         st.header("Informações Básicas da Rede")
         col1, col2 = st.columns(2)
         with col1:
-            st.metric(label="Número de Nós (Vértices)", value=G.number_of_nodes())
+            st.metric(label="Número de Nós (Vértices)", value=G_subsubgraph.number_of_nodes())
         with col2:
-            st.metric(label="Número de Arestas (Ligações)", value=G.number_of_edges())
+            st.metric(label="Número de Arestas (Ligações)", value=Gsubsubgraph.number_of_edges())
 
         st.header("Visualização Interativa da Rede")
         net = Network(height='700px', width='100%', notebook=True, bgcolor="#222222", font_color="white")
-        net.from_nx(G)
+        net.from_nx(G_subsubgraph)
         net.show_buttons(filter_=['physics'])
 
         html_content = net.generate_html()
